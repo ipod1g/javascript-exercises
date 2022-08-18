c0f5cdb04e2234fa9d05ccbb63310cd0b28929a3
Bono <kubonkwan99@gmail.com>
2022-08-18 11:45:58 +0800

    Complete exercise 1,2 and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = (textTest, n) => {
+    if (n < 0 ) return 'ERROR';
 
+    let text = '';
+    for(i=0; i<n; i++){
+        text += textTest;
+    }
+    return text;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    const letterArray = word.split("");
+    let reverseWord = '';
 
+    const reverseLetter = (letterArray) => {
+        for(i=letterArray.length; i>0 ;i--) {
+            reverseWord += letterArray[i-1];
+        }
+        return reverseWord;
+    }
+    reverseLetter(letterArray);
+
+    return reverseWord;
 };
 
 // Do not edit below this line
