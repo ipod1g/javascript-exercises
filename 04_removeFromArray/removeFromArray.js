11e801ce959c0ce220430dfd650633edffca6609
Bono <kubonkwan99@gmail.com>
2022-08-18 12:29:28 +0800

    Complete ex 4
    Interesting to find use of function with extensible parameter
    function(...arg)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(randArray, ...num) {
+    for (const n of num) {
+        let findNum = randArray.indexOf(n);
+        if (findNum > -1){
+            let removedArray = randArray.splice(findNum,1);
+        }
+    }
+    return randArray;
 };
 
 // Do not edit below this line
